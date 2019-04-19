2638e05566e84e0f84560252ce7d7e69a6fff627
Brodur <brodiejgiesbrecht@gmail.com>
2019-04-19 16:34:23 -0500

    Admins and Wizard
    Implemented user priviliges and first run wizard

--- login.lua
+++ login.lua
@@ -18,12 +18,17 @@ local rs = component.redstone
 local wait = 4 -- How long to hold the door open
 local side = sides.west --What side the door is on
 local rsOutput = {max=15, min=0}
-local defaultUsers = {Brodur=true, crassclown=true}
 local users = {}
 local menuOpts = {"User list", "Add User", "Remove User", "Return","End Program"}
 local watching = true
 local configDir = "/home/users.lua"
 
+local privs = {
+  [1] = false, 
+  [2] = true, 
+  admin = true, 
+  user = false 
+}
 
 --- Trims strings
 -- Gets rid of trailing white space or special characters.
@@ -50,12 +55,36 @@ function welcome()
   print("Press any key to enter!")
 end
 
+--- Print Users
+-- Print all of the users
+function printUsers()
+  print("Users:")
+  for user,priv in pairs(users) do
+    print(" - " .. user, priv and "[Admin]" or "[User]")
+  end
+  print("\nPress enter to continue...")
+  term.read()
+end
+
 --- Add Trusted Player
 -- Adds a trusted player to the door.
 -- @param player	The player to add.
-function addTrustedPlayer(player)
+-- @param isAdmin Whether the player should be an admin or not
+function addTrustedPlayer(player, isAdmin)
   if users[player] ~= nil then error("Cannot add a user that already exists!") end
-  users[player] = true
+  users[player] = isAdmin
+end
+
+--- Add trusted player menu
+-- Dialog for adding a trusted player.
+function addTrustedPlayerMenu()
+  term.write("Add a trusted player: ")
+  local player  = trim(term.read())
+  local usrType = menu.dialog("User or Admin?", "User", "Admin")
+  local confirm = menu.dialog("Add \'" .. player .. "\' as " .. usrType==1 and "User" or "Admin" .." to door?", "Confirm", "Cancel")
+  if confirm == 1 then
+    addTrustedPlayer(player, privs[usrType])
+  end
 end
 
 --- Remove Trusted Player
@@ -65,50 +94,50 @@ function removeTrustedPlayer(player)
   users[player] = nil
 end
 
-function manage()
-  local opt = -1
-  while opt ~= #menuOpts do
-    opt = menu.list(menuOpts, "Management Menu")
-    if opt == #menuOpts then watching = false break end
-    if opt == #menuOpts - 1 then break end
-    if opt == 1 then
-      print("Users:\n")
-      for username,_ in pairs(users) do 
-        print(username) 
-      end
-      print("Press enter to continue...")
-      term.read()
-    end
-    if opt == 2 then 
-      term.write("Add a trusted player: ")
-      local player  = trim(term.read())
-      local confirm = menu.dialog("Add \'" .. player .. "\' to door?", "Confirm", "Cancel")
-      if confirm == 1 then
-        addTrustedPlayer(player)
-      end
-    end
-    if opt == 3 then
-      local userList = {"Cancel"}
-      for username,_ in pairs(users) do userList[#userList+1] = username end
-      local select = -1
-      while select ~= 1 do
-        select = menu.list(userList, "Select a user")
-        if select ~= 1 then
-          local player = userList[select]
-          local confirm = menu.dialog("Remove \'" .. player .. "\' from trusted users?", "Confirm", "Cancel")
-          if confirm == 1 then 
-            removeTrustedPlayer(player)
-            table.remove(userList, select)
-          end
-        end
+--- Remove trusted player menu
+-- Dialog to remove a trusted user.
+function removeTrustedPlayerMenu()
+  local userList = {"Cancel"}
+  for username,_ in pairs(users) do userList[#userList+1] = username end
+  local select = -1
+  while select ~= 1 do
+    select = menu.list(userList, "Select a user")
+    if select ~= 1 then
+      local player = userList[select]
+      local confirm = menu.dialog("Remove \'" .. player .. "\' from trusted users?", "Confirm", "Cancel")
+      if confirm == 1 then 
+        removeTrustedPlayer(player)
+        table.remove(userList, select)
       end
     end
   end
 end
 
+--- manage
+-- Menu for various management functions
+function manage()
+  local opt = -1
+  while opt ~= #menuOpts do
+    opt = menu.list(menuOpts, "Management Menu")
+    if opt == #menuOpts then watching = false break end --Exit program
+    if opt == #menuOpts - 1 then break end --Return to login
+    if opt == 1 then 
+      printUsers()
+    end
+    if opt == 2 then 
+      addTrustedPlayerMenu()
+    end
+    if opt == 3 then
+      removeTrustedPlayerMenu()
+    end
+  end
+end
+
+--- Load Config
+-- Loads the configuration from file
 function loadConfig()
   if not fs.exists(configDir) then
-    sz.save(defaultUsers, configDir)
+    noConfig()
   end
   users = sz.load(configDir)
 end
@@ -122,14 +151,16 @@ function program_loaded()
     local match = false
     welcome()
     _,_,_,key,user = event.pull("key_down")
-    if users[user] then
-      if key == 184 then 
+    if users[user] ~= nil then
+      if key == 184 or key == 29 and users[user] then --press R-ALT or UK R-Alt/lcontrol
         computer.beep(200)
         manage()
+        computer.beep(100)
       else
         computer.beep(600)
         print("Welcome", user)
         openClose()
+        computer.beep(500, 1)
       end
     else
       print("Not welcome", user)
@@ -138,6 +169,30 @@ function program_loaded()
   end
   computer.beep(1000)
   sz.save(users, configDir)
+end
+
+--- noConfig
+-- First time or no config present wizard
+-- Adds the user that hits the prompt as admin.
+function noConfig()
+  noConfigBeep()
+  print("No config able to be loaded from: " .. configDir)
+  print("Fisrt run wizard: Please press any key to register your username...")
+  _,_,_,_,user = event.pull("key_down")
+  print("Got: " .. user)
+  addTrustedPlayer(user, privs.admin)
+  printUsers()
+  sz.save(users, configDir)
+end
+
+--- No config beep
+-- Beep function for no config present.
+function noConfigBeep()
+  computer.beep(1000, 0.25)
+  os.sleep(0.1)
+  computer.beep(1000, 0.25)
+  os.sleep(0.1)
+  computer.beep(1000, 0.25)
 end
 
 program_loaded()
